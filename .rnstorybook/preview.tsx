504db499745acd3dc9fbb34f2c7aed43c2097a7a
Ayami Sekiguchi <sekiguchi.a@x-point-1.net>
2026-01-14 09:59:52 +0900

    🦄 refactor: 不要な記述を削除
    preview.tsxのAsyncStorageを削除しました。

--- .rnstorybook/preview.tsx
+++ .rnstorybook/preview.tsx
@@ -1,26 +1,7 @@
 import type { Preview } from '@storybook/react-native';
 import '../global.css';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// AsyncStorageをStorybook環境でモック
-if (typeof AsyncStorage !== 'object' || !AsyncStorage.getItem) {
-  const mockAsyncStorage = {
-    getItem: () => Promise.resolve(null),
-    setItem: () => Promise.resolve(),
-    removeItem: () => Promise.resolve(),
-    clear: () => Promise.resolve(),
-    getAllKeys: () => Promise.resolve([]),
-    multiGet: () => Promise.resolve([]),
-    multiSet: () => Promise.resolve(),
-    multiRemove: () => Promise.resolve(),
-  };
-
-  // グローバルにモックを設定
-  Object.assign(AsyncStorage, mockAsyncStorage);
-}
 
 const preview: Preview = {
-  decorators: [(Story) => <Story />],
   parameters: {
     controls: {
       matchers: {
